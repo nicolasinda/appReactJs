a7788b69238e672b9ab510dc73103ce398f2298c
Nicolas Inda <nicolasinda98@gmail.com>
2022-06-28 23:29:37 -0300

    incorporando navbar

---
+++ src/comoponents/Ejemplo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Ejemplo=()=>{
+    return(
+        <h2>Hola mundo</h2>
+    )
+
+};
+
+export default Ejemplo;
---
+++ src/comoponents/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+const NavBar=()=>{
+return(
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="https://www.facebook.com/">Lucero</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://www.facebook.com/">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.facebook.com/">Carrito</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="..." aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+)
+};
+
+export default NavBar;
