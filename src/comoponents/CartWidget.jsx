fd4a8c01031408b6ac20d17cdd1fe51c14b5578c
Nicolas Inda <nicolasinda98@gmail.com>
2022-07-05 22:51:00 -0300

    contadorCart

--- src/comoponents/CartWidget.jsx
+++ src/comoponents/CartWidget.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+const contador=0;
 const CartWidget=()=>{
     return(
         <>
-      <i className="bi bi-cart3"></i>
+      <i className="bi bi-cart3">{contador}</i>
         </>
     )
 
